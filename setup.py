d7806e5b17621f9f91bb370900bca1fd41742fdb
barakbeilin <barak.beilin@gmail.com>
2020-06-19 17:12:41 +0300

    created const class, couldn't use setup.py correctly

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+######
+setup(
+    name="tDSIN",
+    version="0.0.1",
+    author="BarakBeilin",
+    url='#',
+    author_email='barak.beilin@gmail.com',
+    description=("Convert DSIN project to Pytorch"),
+    license='MIT',
+    package_dir={'': 'src'},
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+    ],
+    zip_safe=False
+)
