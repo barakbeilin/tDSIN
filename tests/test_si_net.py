a7bc500dcc6e17451d3781d63b8713ed0bd5b8e0
barakbeilin <barak.beilin@gmail.com>
2020-06-30 08:05:49 +0300

    si_net tests passes

--- tests/test_si_net.py
+++ tests/test_si_net.py
@@ -16,16 +16,23 @@ class TestSiNet(unittest.TestCase):
         self.assertEqual(tuple(y.shape), (2, 3, 256, 256))
 
     def test_init_weight_to_kaminig(self):
-        
+
         lreul_gain = torch.sqrt(torch.tensor(2.0))  # roughly
+        i = 0
         for param in self.net.parameters():
             if len(param.shape) == 4:
-                fan_in = param.shape[1]
-                kaiming_uniform_bound = lreul_gain * torch.sqrt(torch.tensor(3.0) / fan_in)
+                # param = [channel_out, channel_in , KER_H , KER_L]
+                # fan_in = CHANNEL_IN * KERNEL_SIZE = CHANNEL_IN * KER_H * KER_L
+                fan_in = param.shape[1] * (param.shape[2] * param.shape[3])
+                kaiming_uniform_bound = lreul_gain * torch.sqrt(
+                    torch.tensor(3.0) / fan_in)
+                
                 calc_variance = (2 * kaiming_uniform_bound) ** 2 / 12
-                print(calc_variance)
                 self.assertAlmostEqual(torch.mean(param), 0.0, delta=0.1)
-                self.assertEqual(torch.var(param), calc_variance)
+                print(i)
+                i += 1
+                self.assertAlmostEqual(torch.var(param), calc_variance, delta=0.05)
+
 
 if __name__ == "__main__":
     unittest.main()
