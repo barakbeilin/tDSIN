bb4f5ac9e960e6fb944a896c18b61930d260e787
barakbeilin <barak.beilin@gmail.com>
2020-07-06 11:30:56 +0300

    si_ae tests checking for dimensionality pass.

--- src/dsin/ae/si_ae.py
+++ src/dsin/ae/si_ae.py
@@ -7,13 +7,13 @@ from dsin.ae.probclass import ProbClassifier
 from dsin.ae.importance_map import ImportanceMapMult
 from dsin.ae.si_net import SiNet, SiNetChannelIn
 from dsin.ae.si_finder import SiFinder
-from dsin.ae.kitti_normalizer import ChangeImageStatsToKitti, ChangeStates
+from dsin.ae.kitti_normalizer import ChangeImageStatsToKitti, ChangeState
 
 
-class SideInformationEncoder(nn.Module):
+class SideInformationAutoEncoder(nn.Module):
     def __init__(self, use_side_infomation: SiNetChannelIn):
         super().__init__()
-
+        self.use_side_infomation = use_side_infomation
         self.enc = Encoder.create_module_from_const()
         self.dec = Decoder.create_module_from_const()
 
@@ -35,54 +35,74 @@ class SideInformationEncoder(nn.Module):
             receptive_field=config.quantizer_kernel_w_h,
         )
 
-        self.si_net = SiNet(in_channels=use_side_infomation, use_eye_init=True)
+        self.si_net = SiNet(in_channels=self.use_side_infomation, use_eye_init=True)
+
         self.si_finder = SiFinder()
 
-        self.noramlize = ChangeImageStatsToKitti(direction=ChangeStates.NORMALIZE)
-        self.denoramlize = ChangeImageStatsToKitti(direction=ChangeStates.DENORMALIZE)
+        self.noramlize = ChangeImageStatsToKitti(direction=ChangeState.NORMALIZE)
+        self.denoramlize = ChangeImageStatsToKitti(direction=ChangeState.DENORMALIZE)
 
-    def forward(
-        self, x: torch.tensor, y: torch.tensor, new_y: bool, train_only_ae: bool
-    ):
+    def forward(self, x: torch.tensor, y: torch.tensor):
 
-        # N| nof inpput Quantization Channels + 1|H|W
+        # N| nof inpput Quantization Channels + 1|H/8|W/8
         # TODO: DELETE AND PASS INTO importance_map_layer DIRECTLY
         x_enc = self.enc(x)
 
-        # improtance map - N1HW
-        # x_post_map - N|nof input quantization channels|H|W
+        # improtance map - N|nof input quantization channels|H/8|W/8
+        # x_post_map - N|nof input quantization channels|H/8|W/8
         importance_map_mult_weights, x_post_map = self.importance_map_layer(x_enc)
 
-        # x_quantizer(a.k.a z-hat) - N|nof input Quntization Channels|H|W
-        x_quantizer = self.quantizer(x_post_map)
+        # N|nof input Quntization Channels|H|W
+        # x_soft - data values are from centers alphabet, gradients from softmax
+        # (2) x_hard - not used, include data of x_soft without gardients.
+        # x_index_of_closest_center- indexes of the center values inside x_soft.data
+        x_quantizer_soft, _, x_quantizer_index_of_closest_center = self.quantizer(
+            x_post_map
+        )
 
         # create a copy without gradient to prevent multiple gradients flowing
         # into the importance map
         # x_pc - -5dims of size:
         #   N|nof Quantization Centers|nof input quantization channels |H|W|
-        x_pc = self.prob_classif(x_quantizer.detach())
+        x_pc = self.prob_classif(x_quantizer_soft.detach())
 
         # N|3|H|W
-        x_dec = self.dec(x_quantizer)
-        normalized_x_dec = self.noramlize(x_dec)
+        x_dec = self.dec(x_quantizer_soft)
 
-        # N|3|H|W
-        # TODO: DELETE AND PASS INTO cat DIRECTLY
-        normalized_y_syn = self.noramlize(self.calc_y_syn(y=y, x_dec=normalized_x_dec))
+        if self.use_side_infomation == SiNetChannelIn.WithSideInformation:
+            normalized_x_dec = self.noramlize(x_dec)
 
-        # N|6|H|W, concat on channel dim
-        # TODO: DELETE AND PASS INTO SI_NET DIRECTLY
-        normalized_x_dec_y_syn = torch.cat((normalized_x_dec, normalized_y_syn), dim=1)
+            # N|3|H|W
+            # TODO: DELETE AND PASS INTO cat DIRECTLY
+            normalized_y_syn = self.noramlize(
+                self.calc_y_syn(y=y, x_dec=normalized_x_dec)
+            )
 
-        # N|3|H|W
-        x_reconstructed = self.denoramlize(self.si_net(normalized_x_dec_y_syn))
-        return x_reconstructed, x_dec, x_pc
+            # N|6|H|W, concat on channel dim
+            # TODO: DELETE AND PASS INTO SI_NET DIRECTLY
+            normalized_x_dec_y_syn = torch.cat(
+                (normalized_x_dec, normalized_y_syn), dim=1
+            )
+
+            # N|3|H|W
+            x_reconstructed = self.denoramlize(self.si_net(normalized_x_dec_y_syn))
+        else:
+            x_reconstructed = None
+
+        return (
+            x_reconstructed,  # for total loss
+            x_dec,  # for auto-encoder loss
+            x_pc,  # for probability classifier loss
+            importance_map_mult_weights,  # for probability classifier loss
+            x_quantizer_index_of_closest_center,  # for probability classifier loss
+        )
 
     def calc_y_syn(self, y, x_dec):
         # stop gradients in si-finder and calculation of y_dec
         with torch.no_grad():
             _, y_post_map = self.importance_map_layer(self.enc(y))
-            y_dec = self.dec(self.quantizer(y_post_map))
+            y_quantizer_soft, _, _ = self.quantizer(y_post_map)
+            y_dec = self.dec(y_quantizer_soft)
 
             # y_syn N|3|H|W
-            return self.si_finder.create_y_syn(x_dec=x_dec, y_dec=y_dec)
+            return self.si_finder.create_y_syn(x_dec=x_dec, y_dec=y_dec, y=y)
---
+++ tests/test_si_ae.py
@@ -0,0 +1,50 @@
+import unittest
+from dsin.ae.si_ae import *
+from dsin.ae import config
+
+
+class TestSIAE(unittest.TestCase):
+    def test_flow_with_si(self):
+        si_ae = SideInformationAutoEncoder(SiNetChannelIn.WithSideInformation)
+        denoramlize = ChangeImageStatsToKitti(direction=ChangeState.DENORMALIZE)
+
+        # change image stats to mock kitti image
+        x = denoramlize(torch.randn(1, 3, 192, 144))
+        y = denoramlize(torch.randn(1, 3, 192, 144))
+        (
+            x_reconstructed,
+            x_dec,
+            x_pc,
+            importance_map_mult_weights,
+            x_quantizer_index_of_closest_center,
+        ) = si_ae(x=x, y=y)
+
+
+
+
+
+        self.assertEqual(tuple(x_reconstructed.shape), (1, 3, 192, 144))
+        self.assertEqual(tuple(x_dec.shape), (1, 3, 192, 144))
+        self.assertEqual(tuple(x_pc.shape), (1, 6, 32, 192/8, 144/8))
+        self.assertEqual(tuple(importance_map_mult_weights.shape), (1, 32, 192/8, 144/8))
+        self.assertEqual(tuple(x_quantizer_index_of_closest_center.shape), (1, 32, 192/8, 144/8))
+
+    def test_flow_without_si(self):
+        si_ae = SideInformationAutoEncoder(SiNetChannelIn.NoSideInformation)
+        denoramlize = ChangeImageStatsToKitti(direction=ChangeState.DENORMALIZE)
+        x = denoramlize(torch.randn(1, 3, 192, 144))
+        (
+            x_reconstructed,
+            x_dec,
+            x_pc,
+            importance_map_mult_weights,
+            x_quantizer_index_of_closest_center,
+        ) = si_ae(x=x, y=None)
+        self.assertEqual(tuple(x_dec.shape), (1, 3, 192, 144))
+        self.assertEqual(tuple(x_pc.shape), (1, 6, 32, 192/8, 144/8))
+        self.assertEqual(tuple(importance_map_mult_weights.shape), (1, 32, 192/8, 144/8))
+        self.assertEqual(tuple(x_quantizer_index_of_closest_center.shape), (1, 32, 192/8, 144/8))
+
+
+if __name__ == "__main__":
+    unittest.main()
