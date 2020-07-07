adb8c22dc39396fa0e7e771ff830a57c5cd44029
barakbeilin <barak.beilin@gmail.com>
2020-07-07 14:26:27 +0300

    data_loader moved from nb to class + passing tests. updated env.yaml for tfastai v1

---
+++ tests/test_data_loader.py
@@ -0,0 +1,23 @@
+import unittest
+from dsin.ae.data_manager.data_loader import (
+    SideinformationImageImageList, ImageSiTuple)
+
+
+class TestDataLoader(unittest.TestCase):
+    def setUp(self):
+        self.image_list = SideinformationImageImageList.from_csv(
+            path="/mnt/code/repos/tDSIN/src/dsin/data",
+            csv_name="tiny_KITTI_stereo_train.txt")
+
+    def test_si_image_list_work(self):
+        self.assertEqual(len(self.image_list), 50)
+        self.assertEqual(str(
+            self.image_list[0].__class__),
+            "<class 'dsin.ae.data_manager.data_loader.ImageSiTuple'>")
+        self.assertEqual(tuple(self.image_list[0].img.shape), (3, 370, 1226))
+        self.assertEqual(
+            tuple(self.image_list[0].si_img.shape), (3, 370, 1226))
+
+
+if __name__ == "__main__":
+    unittest.main()
