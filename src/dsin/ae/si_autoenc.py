8f2712f5bd1b87571c22d1ac610b63fa7cf64f0f
Ubuntu <barak.beilin@gmail.com>
2020-08-15 12:12:08 +0000

    .

--- src/dsin/ae/si_autoenc.py
+++ src/dsin/ae/si_autoenc.py
@@ -14,18 +14,15 @@ class SideInformationAutoEncoder(nn.Module):
     def __init__(self,base_line_auto_enc: nn.Module):
         super().__init__()
         self.ae = base_line_auto_enc
+
         self.true_tuple_loss_false_just_out = True
-        self.importance_map_layer = 
 
         self.si_net = SiNet(
             in_channels=SiNetChannelIn.WithSideInformation, use_eye_init=False)
 
         self.si_finder = SiFinder()
 
-        self.noramlize = ChangeImageStatsToKitti(
-            direction=ChangeState.NORMALIZE)
-        self.denoramlize = ChangeImageStatsToKitti(
-            direction=ChangeState.DENORMALIZE)
+        
 
     def forward(self, x: torch.tensor, y: torch.tensor):
         (_,  # for total loss
@@ -38,12 +35,12 @@ class SideInformationAutoEncoder(nn.Module):
        
         y_syn = normalized_y_syn = None
         
-        normalized_x_dec = self.noramlize(x_dec)
+        normalized_x_dec = self.ae.noramlize(x_dec)
         # N|3|H|W
         # TODO: DELETE AND PASS INTO cat DIRECTLY
         y_syn = self.calc_y_syn(y= y * config.open_image_normalization,
                 normalized_x_dec=normalized_x_dec)
-        normalized_y_syn = self.noramlize(y_syn)
+        normalized_y_syn = self.ae.noramlize(y_syn)
 
         # N|6|H|W, concat on channel dim
         # TODO: DELETE AND PASS INTO SI_NET DIRECTLY
@@ -81,9 +78,9 @@ class SideInformationAutoEncoder(nn.Module):
 
         # stop gradients in si-finder and calculation of y_dec
         with torch.no_grad():
-            _, y_post_map = self.importance_map_layer(self.enc(y))
-            y_quantizer_soft, _, _ = self.quantizer(y_post_map)
-            normalized_y_dec = self.noramlize(self.dec(y_quantizer_soft))
+            _, y_post_map = self.ae.importance_map_layer(self.ae.enc(y))
+            y_quantizer_soft, _, _ = self.ae.quantizer(y_post_map)
+            normalized_y_dec = self.ae.noramlize(self.ae.dec(y_quantizer_soft))
 
             # y_syn N|3|H|W
             return self.si_finder.create_y_syn(x_dec=normalized_x_dec, y_dec=normalized_y_dec, y=y)
