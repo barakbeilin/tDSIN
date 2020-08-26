ec26b41ddc5ffc4cb4e1cd77800c6a2b1b85ff32
barakbeilin <38550676+barakbeilin@users.noreply.github.com>
2020-08-27 02:26:04 +0300

    Update pbar_cb_utils.py
    change listy

--- src/dsin/train_utils/visibilty_utils/pbar_cb_utils.py
+++ src/dsin/train_utils/visibilty_utils/pbar_cb_utils.py
@@ -73,7 +73,7 @@ class AverageMetric(Callback):
 
     def on_batch_end(self, last_output, last_target, **kwargs):
         "Update metric computation with `last_output` and `last_target`."
-        if not is_list(last_target): last_target=[last_target]
+        if not isinstance(last_target, (list,tuple)): last_target=[last_target]
         self.count += last_target[0].size(0) # batch size
         val = self.func(last_output, last_target[0])
         self.val += last_target[0].size(0) * val.detach().cpu()
