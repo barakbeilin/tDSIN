00cc03c7ad966cf3200d33524f827e69b149ad4f
Ubuntu <barak.beilin@gmail.com>
2020-08-26 18:45:20 +0000

    .

--- src/dsin/train_utils/save_utils.py
+++ src/dsin/train_utils/save_utils.py
@@ -4,7 +4,7 @@ import datetime
 class FileNameGenerator:
     @staticmethod
     def create_fname(fname_suffix: str) -> str:
-        curr_datetime = str(datetime.datetime.now().strftime('%Y-%m-%d-%H-%M'))
+        curr_datetime = str(datetime.datetime.now().strftime('%Y-%m-%d'))
         return f'{curr_datetime}{fname_suffix}'
 
 
